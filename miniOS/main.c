1174ee9a512b66f389f02dad9ff88d4b966ab128
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-14 09:46:29 +0100

    Initial Commit

---
+++ miniOS/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  miniOS
+//
+//  Created by Alexi Canesse on 14/04/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
