d9c2212bf7113a26167d2d619707ca5d930ab0cd
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-15 13:34:55 +0100

    started the implementation of vCPUs

--- miniOS/vCPU.c
+++ miniOS/vCPU.c
@@ -4,5 +4,20 @@
 //
 //  Created on 15/04/2022.
 //
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "vCPU.h"
+
+
+vCPU* create_vCPU(void){
+    pthread_t *thread = malloc(sizeof(thread));
+    
+    vCPU *cpu = (vCPU*) malloc(sizeof(vCPU));
+    
+    pthread_create(thread, NULL, idle, NULL); //crée un thread (vCPU) et le suspend jusqu'à reception d'un signal
+    cpu->pthread = thread;
+    
+    return cpu;
+}
--- miniOS/vCPU.h
+++ miniOS/vCPU.h
@@ -8,6 +8,40 @@
 #ifndef vCPU_h
 #define vCPU_h
 
-#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+
+struct vCPU{ //je definie une strcut au cas où on veut y ajouter des choses
+    pthread_t *pthread;
+};
+typedef struct vCPU vCPU;
+
+struct uThread{ //a enrichir suivant le besoin
+    int uPID; //sert à identifier le thread
+};
+typedef struct uThread uThread;
+
+#warning selon l'exemple tu tableau il faut ajouter un argument mais je ne sais pas lequel
+vCPU* create_vCPU(void);
+int destruct_vCPU(vCPU* cpu); //le int sert aux messages d'erreur
+
+#warning arguments à definir
+uThread* create_uThread(void);
+int destruct_uThread(uThread* thread);
+int yield(uThread* thread);
+
+#warning il faut limite le sigsuspend à un signal user qui servira à ca (plus safe)
+void *idle(void* param){ //suspend jusqu'à ce qu'un signal soit recu.
+    sigset_t sigmask;
+    sigemptyset(&sigmask);
+    
+    sigsuspend(&sigmask);
+    return NULL;
+}
+
+
+
+
 
 #endif /* vCPU_h */
