539e53292377907bd042578282f3d07502629d25
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-16 18:17:15 +0100

    intial commit

---
+++ miniOS/miniOS_private.h
@@ -0,0 +1,41 @@
+//
+//  miniOS_private.h
+//  miniOS
+//
+//  Created on 16/04/2022.
+//
+
+
+//this file contains the definitions that are not meant to be used by the user
+
+#ifndef miniOS_private_h
+#define miniOS_private_h
+
+//vcpu
+struct vCPU{
+    pthread_t *pthread;
+    struct vCPU *next; //used to make lists
+};
+typedef struct vCPU vCPU;
+
+void *init(void* param);
+void *idle(void* param);
+void switch_process(int signum, siginfo_t *info, void *ptr);
+
+//scheduler
+struct queue_element{
+    uThread *element;
+    struct queue_element *next;
+};
+typedef struct queue_element queue_element;
+
+struct uThread_queue{
+    queue_element *first;
+    queue_element *last;
+};
+typedef struct uThread_queue uThread_queue;
+
+int scheduler_add_thread(uThread *thread); //return -1 if fails and sets errno. Otherwise, return 0
+
+
+#endif /* miniOS_private_h */
