0475a822cddd8ef5f43e4990b4e975c8516158f5
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-16 10:18:35 +0100

    implemented create_vCPU and destruct_vCPU following Alain's advice

--- miniOS/vCPU.c
+++ miniOS/vCPU.c
@@ -11,13 +11,42 @@
 #include "vCPU.h"
 
 
-vCPU* create_vCPU(void){
-    pthread_t *thread = malloc(sizeof(thread));
+vCPU *vCPUs = NULL; //list of all running vCPUs
+
+
+int create_vCPU(int nbr_vCPU){
+    while(nbr_vCPU--){
+        pthread_t *thread = (pthread_t*) malloc(sizeof(thread));
+        vCPU *cpu = (vCPU*) malloc(sizeof(vCPU));
     
-    vCPU *cpu = (vCPU*) malloc(sizeof(vCPU));
+        if(thread == NULL || cpu == NULL)//if malloc failed
+            return -1; //malloc already sets errno correctly
+        
+        //create a thread (vCPU) and suspend it until a signal wakes it up
+        if(pthread_create(thread, NULL, idle, NULL)) //if return 0, it worked
+            return -1; //pthread_create already sets errno correctly
+            
+        cpu->pthread = thread;
+        
+        //add the vCPU to the list of vCPUs
+        cpu->next = vCPUs;
+        vCPUs = cpu;
+    }
     
-    pthread_create(thread, NULL, idle, NULL); //crée un thread (vCPU) et le suspend jusqu'à reception d'un signal
-    cpu->pthread = thread;
-    
-    return cpu;
+    return 0;
+}
+
+int destruct_vCPU(int nbr_vCPU){
+    while(nbr_vCPU--){
+        if(vCPUs == NULL) //There isn't any vCPU left to delete
+            return -1; //we were not able to delete enough vCPUs
+        
+        vCPU *cpu_buffer = vCPUs;
+        vCPUs = vCPUs->next; //the first vCPU is removed from the list
+        
+        //free the memory
+        free(cpu_buffer->pthread);
+        free(cpu_buffer);
+    }
+    return 0;
 }
--- miniOS/vCPU.h
+++ miniOS/vCPU.h
@@ -12,19 +12,20 @@
 #include <unistd.h>
 #include <signal.h>
 
-struct vCPU{ //je definie une strcut au cas où on veut y ajouter des choses
+struct vCPU{
     pthread_t *pthread;
+    struct vCPU *next; //used to make lists
 };
 typedef struct vCPU vCPU;
 
-struct uThread{ //a enrichir suivant le besoin
-    int uPID; //sert à identifier le thread
+struct uThread{
+    int uPID; //thread id
 };
 typedef struct uThread uThread;
 
-#warning selon l'exemple tu tableau il faut ajouter un argument mais je ne sais pas lequel
-vCPU* create_vCPU(void);
-int destruct_vCPU(vCPU* cpu); //le int sert aux messages d'erreur
+//create or destruct nbr_vCPU vCPUs 
+int create_vCPU(int nbr_vCPU); //returns 0 unless it fails. errno is set if it fails
+int destruct_vCPU(int nbr_vCPU); //returns 0 unless it fails. errno is set if it fails
 
 #warning arguments à definir
 uThread* create_uThread(void);
@@ -32,7 +33,7 @@ int destruct_uThread(uThread* thread);
 int yield(uThread* thread);
 
 #warning il faut limite le sigsuspend à un signal user qui servira à ca (plus safe)
-void *idle(void* param){ //suspend jusqu'à ce qu'un signal soit recu.
+void *idle(void* param){ //suspends until a signal is received
     sigset_t sigmask;
     sigemptyset(&sigmask);
     
