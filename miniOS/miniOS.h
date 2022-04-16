cccc3782bef2d385b5a80bb28290f88bb50962fa
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-16 18:17:58 +0100

    added the propre include to make this file the only file the user needs to import

--- miniOS/miniOS.h
+++ miniOS/miniOS.h
@@ -2,11 +2,17 @@
 //  miniOS.h
 //  miniOS
 //
-//  Created by Alexi Canesse on 14/04/2022.
+//  Created on 14/04/2022.
 //
+
+//This is the only header the user will import
 
 #ifndef miniOS_h
 #define miniOS_h
+
+#include "vCPU.h"
+#include "scheduler.h"
+#include "memory.h"
 
 
 #endif /* miniOS_h */
