4b7987a0a485d1eb0e9aba3ba994e1130583747c
alexicanesse <alexi.canesse@protonmail.ch>
2022-04-18 18:19:41 +0100

    corrected some memory leaks

--- miniOS/vCPU.c
+++ miniOS/vCPU.c
@@ -18,7 +18,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 #include "vCPU.h"
 #include "scheduler.h"
@@ -29,21 +30,29 @@
 
 vCPU *vCPUs = NULL; //list of all running vCPUs
 uThread *uThreads = NULL; //list of all running uThreads
+extern scheduler_type scheduler;
 
 #warning TODO initialize at right time
 ucontext_t *current_context = NULL; //in stack so it is thread-specific
 
 int create_vCPU(int nbr_vCPU){
     while(nbr_vCPU--){
-        pthread_t *thread = (pthread_t*) malloc(sizeof(thread));
-        vCPU *cpu = (vCPU*) malloc(sizeof(vCPU));
-    
-        if(thread == NULL || cpu == NULL)//if malloc failed
+        pthread_t *thread = (pthread_t*) malloc(sizeof(pthread_t));
+        if(thread == NULL)//if malloc failed
             return -1; //malloc already sets errno correctly
         
+        vCPU *cpu = (vCPU*) malloc(sizeof(vCPU));
+        if(cpu == NULL){//if malloc failed
+            free(thread);
+            return -1; //malloc already sets errno correctly
+        }
+        
         //create a thread (vCPU) and suspend it until a signal wakes it up
-        if(pthread_create(thread, NULL, init, NULL)) //if return 0, it worked
+        if(pthread_create(thread, NULL, init, NULL)){ //if return 0, it worked
+            free(thread);
+            free(cpu);
             return -1; //pthread_create already sets errno correctly
+        }
             
         cpu->pthread = thread;
         
@@ -73,7 +82,7 @@ int destruct_vCPU(int nbr_vCPU){
 }
 
 int create_uThread(void (*func)(void), int argc, const char * argv[]){
-    uThread *thread = (uThread *) malloc(sizeof(uThread*));
+    uThread *thread = (uThread *) malloc(sizeof(uThread));
     if(thread == NULL) //if malloc failed
         return -1; //malloc already sets errno correctly
     
@@ -84,15 +93,20 @@ int create_uThread(void (*func)(void), int argc, const char * argv[]){
     
     ucontext_t *context = (ucontext_t *) malloc(sizeof(ucontext_t));
     thread->context = context;
-    if(context == NULL) //if malloc failed
+    if(context == NULL){ //if malloc failed
+        free(thread);
         return -1; //malloc already sets errno correctly
+    }
     
     
     
     char *stack = (char*) malloc(thread->stack_size);
     thread->stack = stack;
-    if(stack == NULL) //if malloc failed
+    if(stack == NULL){ //if malloc failed
+        free(thread);
+        free(context);
         return -1; //malloc already sets errno correctly
+    }
     
     //set up the context
     errno = 0; //we must check errno because makecontext does not return a value
@@ -102,6 +116,9 @@ int create_uThread(void (*func)(void), int argc, const char * argv[]){
 #warning I have some doubts about the arguments
     makecontext(context, func, argc, argv);
     if(errno != 0){ //makecontext failed
+        free(thread);
+        free(context);
+        free(stack);
         fprintf(stderr, "%s", strerror(errno)); //prints the error message
         return -1;
     }
@@ -152,32 +169,33 @@ int yield(uThread* thread){
 
 void *init(void* param){ //suspends until a signal is received
     //set up a signal handler for SIGUSR1 in order to let the scheduler tell us to switch to the next process
-    static struct sigaction _sigact;
+    struct itimerval it;
+    it.it_interval.tv_sec = scheduler.quantum;
+    it.it_interval.tv_usec = 0;
+    it.it_value = it.it_interval;
+    if (setitimer(ITIMER_REAL, &it, NULL) )
+        perror("Problème de setitiimer");
 
+    static struct sigaction _sigact;
     memset(&_sigact, 0, sizeof(_sigact));
     _sigact.sa_sigaction = switch_process;
     _sigact.sa_flags = SA_SIGINFO;
 
-    sigaction(SIGUSR1, &_sigact, NULL);
-
-    
-#warning TODO schedule init
-    idle(NULL); //we idle until we are told to schedule a thread
+    sigaction(SIGALRM, &_sigact, NULL);
     return NULL;
 }
 
 #warning il faut limite le sigsuspend à un signal user qui servira à ca (plus safe) le sigusr1
-void *idle(void* param){
+void idle(void){
     sigset_t sigmask;
     sigemptyset(&sigmask);
     
     while(1) //the function wont terminate
         sigsuspend(&sigmask); //there is no need to waste CPU cycles while we wait
-    
-    return NULL;
 }
 
 void switch_process(int signum, siginfo_t *info, void *ptr){
+    printf("here\n");
     uThread *thread = next_to_schedule();
     
     ucontext_t *past_context = current_context;
